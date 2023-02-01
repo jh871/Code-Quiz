0270e43cd5552e46492ef7d633649cd48d7f25fe
jh871 <jennyh871@gmail.com>
2023-02-01 23:15:22 +0000

    added test questions

---
+++ assets/js/questions.js
@@ -0,0 +1,50 @@
+const questions =  [
+    {
+        questionNumber: 1,
+        question: "Question one?",
+        correctAnswer: "This is option four",
+        option1: "This is option one",
+        option2: "This is option two",
+        option3: "This is option three", 
+        option4: "This is option four", 
+    },
+    {
+        questionNumber: 2,
+        question: "Question two?",
+        correctAnswer: "This is the correct answer",
+        option1: "This is option one",
+        option2: "This is option two",
+        option3: "This is option three", 
+        option4: "This is option four", 
+    },
+    {
+        questionNumber: 3,
+        question: "Question three?",
+        correctAnswer: "This is the correct answer",
+        option1: "This is option one",
+        option2: "This is option two",
+        option3: "This is option three", 
+        option4: "This is option four", 
+    },
+    {
+        questionNumber: 4,
+        question: "Question four?",
+        correctAnswer: "This is the correct answer",
+        option1: "This is option one",
+        option2: "This is option two",
+        option3: "This is option three", 
+        option4: "This is option four", 
+    },
+    {
+        questionNumber: 5,
+        question: "Question five?",
+        correctAnswer: "This is the correct answer",
+        option1: "This is option one",
+        option2: "This is option two",
+        option3: "This is option three", 
+        option4: "This is option four", 
+    }
+];
+
+
+
