9ed79c64e19d6f35dad8c7228b3a57e67d5099e9
jh871 <jennyh871@gmail.com>
2023-02-04 19:08:23 +0000

    changes to logic to display choices

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,20 +1,3 @@
-//test logic to loop through and display text for questions
-questionsArr.forEach(function(question, index, arr){
-    // console.log(arr[index].questionNumber + ") " + arr[index].question);
-    // console.log(arr[index].correctAnswer);
-    // console.log(arr[index].option1);
-    // console.log(arr[index].option2);
-    // console.log(arr[index].option3);
-    // console.log(arr[index].option4);
-})
-
-
-//selects random question
-function randomQ() {
-let random = Math.floor(Math.random() * questionsArr.length);
-console.log(questionsArr[random].questionNumber)
-console.log(questionsArr[random].question);
-}
 
 // grabbing elements that will need manipulating
 //startscreen
@@ -31,10 +14,8 @@ let feedbackDiv = document.querySelector("#feedback");
 let endscreenDiv = document.querySelector("#end-screen");
 
 
-
-
 //init 
-// let questionCount = 0;
+let questionCount = 0;
 
 // //function for if selected answer is wrong
 // function wrongAnswer(){
@@ -63,36 +44,77 @@ let endscreenDiv = document.querySelector("#end-screen");
 // question number = questions.index[i + 1]
 
 
-// pulling question title from questionsArr
-for (let i = 0; i < questionsArr.length; i++){
-    questionTitle.textContent = (questionsArr[i].question);
-};
-// x = 5, i = 4
-//generating 4 options
-// const numberofOptions = [1, 2, 3, 4]
-questionsArr.forEach(function(arr) {
+//questionsindex === questions.length
+// let qNums = [];
+
+// for (let i = 0; i < qsArr.length; i++){
+// qNums[i] = qsArr[i].questionNumber;
+// }
+// console.log("Q nums: "+ qNums);
+
+
+//map questionNumber to an array,
+//loop through qNum array?
+
+// let nextBtn = document.createElement("button");
+//     nextBtn.textContent = ("Next");
+//     feedbackDiv.appendChild(nextBtn);
+
+//loop to label choices
+let nextQ = qsArr.forEach(function(arr) {
+    choicesDiv.innerHTML="";
+    questionCount++
+    console.log(questionCount);
+
+    questionTitle.textContent = arr.question;
+
+    let choiceSet = document.createElement("div");
+    choiceSet.classList.add("choices-set");
+    choicesDiv.appendChild(choiceSet)
+    choiceSet.setAttribute("id", "set"+arr.questionNumber)
+
+    for (let i = 0; i< arr.options.length; i++) {
+        let buttonEl = document.createElement("button");
+        buttonEl.id = ("button"+[i]);
+        buttonEl.className = ("button");
+
+    // adding labels to buttons from options
+        buttonEl.textContent = arr.options[i];
+        choiceSet.appendChild(buttonEl);
+        buttonEl.addEventListener("click", function(event) {
+        if (event.target.textContent === arr.correctAnswer){
+            feedbackDiv.textContent = "Correct!";
+        } else if (event.target.textContent !== arr.correctAnswer) {
+            feedbackDiv.textContent = "Wrong!"
+        };
+        //and then generate next btn
+        //run nxtQ function
     
-for (let i = 0; i< arr.options.length; i++) {
-    let buttonEl = document.createElement("button");
-    buttonEl.id = ("button"+[i]);
-    buttonEl.className = ("button");
-
-// adding labels to buttons from options
-
-    buttonEl.textContent = arr.options[i];
-
-
-    choicesDiv.appendChild(buttonEl);
-
-    buttonEl.addEventListener("click", function(event) {
-    if (event.target.textContent === arr.correctAnswer){
-        feedbackDiv.textContent = "Correct!";
-    } else {
-        feedbackDiv.textContent = "Wrong!"
+        if (questionCount <= qsArr.length){
+            nextBtn.addEventListener("click", function(event) {
+            event.preventDefault
+            nextQ();  
+            })} else if (questionCount === qsArr.length){
+                endOfQuiz();
+            }
+        
     
-    
-    }});
-}});
+    });//eventlistener on choice button
+}//for loop through options
+});//qsArr foreach
+
+
+
+//start button
+let startBtn = document.querySelector("#start");
+
+startBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    startscreenDiv.classList.replace("show", "hide");
+    questionsDiv.classList.replace("hide", "show");
+    feedbackDiv.classList.replace("hide", "show");
+});
+
 
 
 // //function for if answer is correct
@@ -103,12 +125,60 @@ for (let i = 0; i< arr.options.length; i++) {
 // };
 
 
-//start button
-let startBtn = document.querySelector("#start");
+// check if correct function
+function checkAnswer(event) {
+    
+    if (event.target.textContent === arr.correctAnswer){
+        feedbackDiv.textContent = "Correct!";
+    } else {
+        feedbackDiv.textContent = "Wrong!"
+    }
+}
+//call
+// choiceBtn.addEventListener("click", checkAnswer());
 
-startBtn.addEventListener("click", function(event){
-    event.preventDefault();
-    startscreenDiv.classList.replace("show", "hide");
-    questionsDiv.classList.replace("hide", "show");
-});
 
+
+
+
+
+
+
+//Other stuff
+//init 
+// let questionCount = 0;
+
+// //function for if selected answer is wrong
+// function wrongAnswer(){
+// feedbackDiv.textContent = "Wrong!"
+// secondsLeft = secondsLeft - 10;
+// questionCount++;
+// //generate next question
+// };
+
+
+// //function for if answer is correct
+// function correctAnswer() {
+//     feedbackDiv.textContent = "Correct!"
+//     questionCount++;
+//     //generate next question
+// };
+
+
+// //loop for end of game
+// if (questionCount = 5){
+//     questionsDiv.className.replace("show", "hide");
+//     endscreenDiv.className.replace("hide","show");
+
+//     let score = secondsRemaining;
+// };
+
+// start quiz
+// function startQuiz() {
+//     startscreenDiv.className.replace("show", "hide");
+//     questionsDiv.className.replace("hide", "show");
+//     }
+
+
+//questions array = [random(), random(), random(), random()]
+// question number = questions.index[i + 1]
